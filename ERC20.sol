41e9b58448694d7df40f4b101cb4d4285ba4503d
Divya Parkash Singh Jasrotia <108604872+jasrotia15@users.noreply.github.com>
2024-09-30 15:16:29 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -7,8 +7,8 @@ contract ERC20 {
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance; 
     
-    string public name = "akshit";
-    string public symbol = "GRANA";
+    string public name = "divya";
+    string public symbol = "DIV";
     uint8 public decimals = 18;
 
 	event Transfer(address indexed from, address indexed to, uint value);
